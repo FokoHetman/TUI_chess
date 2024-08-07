d24461c63be218151f734a5d9d56ccf692de26a0
FokoHetman <paprykkania@gmail.com>
2024-08-07 14:31:01 +0200

    I forgor

--- src/target/debug/build/libc-ced50d587bc01071/build_script_build-ced50d587bc01071.d
+++
@@ -1,5 +0,0 @@
-/home/foko/Projects/Chess/src/target/debug/build/libc-ced50d587bc01071/build_script_build-ced50d587bc01071: /home/foko/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
-
-/home/foko/Projects/Chess/src/target/debug/build/libc-ced50d587bc01071/build_script_build-ced50d587bc01071.d: /home/foko/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
-
-/home/foko/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs:
