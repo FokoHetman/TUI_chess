63bdb941e373255b748d708c815babb6c65a2866
FokoHetman <paprykkania@gmail.com>
2024-08-07 21:15:07 +0200

    we changing flake system once again!!

--- flake.nix
+++ flake.nix
@@ -12,20 +12,6 @@
       in
       {
         formatter = pkgs.alejandra;
-        packages.default = let 
-          mf = (pkgs.lib.importTOML ./Cargo.toml).package;
-        in
-          pkgs.rustPlatform.buildRustPackage rec {
-            pname = mf.name;
-            version = mf.version;
-            src = pkgs.lib.cleanSource ./.;
-
-            cargoLock.lockFile = ./Cargo.lock;
-            
-            CONFIG = "{}";  # not used, somewhere int eh futurr
-
-            cargoHash = "sha256-SkFGStZShqocYwzyU7ylaQZ2+YRmHNCUqkCAvwFt1+c=";#nixpkgs.lib.fakeHash;
-            #cargoSha256 = nixpkgs.lib.fakeSha256;
-          };
+        packages.default = pkgs.callPackage ./. { };
       });
 }
