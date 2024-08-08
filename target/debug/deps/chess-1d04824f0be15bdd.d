23ef482b7fd5645cfcd460fb9d054f4f55223ae9
FokoHetman <paprykkania@gmail.com>
2024-08-08 16:21:53 +0200

    remove binaries

--- target/debug/deps/chess-1d04824f0be15bdd.d
+++
@@ -1,9 +0,0 @@
-/home/foko/Projects/Chess/target/debug/deps/chess-1d04824f0be15bdd: src/main.rs src/Handlers/mod.rs src/Handlers/Control.rs src/Handlers/Control/Board.rs src/Handlers/Control/Board/common.rs
-
-/home/foko/Projects/Chess/target/debug/deps/chess-1d04824f0be15bdd.d: src/main.rs src/Handlers/mod.rs src/Handlers/Control.rs src/Handlers/Control/Board.rs src/Handlers/Control/Board/common.rs
-
-src/main.rs:
-src/Handlers/mod.rs:
-src/Handlers/Control.rs:
-src/Handlers/Control/Board.rs:
-src/Handlers/Control/Board/common.rs:
